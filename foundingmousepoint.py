56ab4d87b772527be7eeb4bb30cf64808cdbc9fc
praveenk8022782-a11y <praveenk8022782@gmail.com>
2026-05-16 13:07:47 +0530

    Add files via upload

---
+++ foundingmousepoint.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+
+time.sleep(5)
+x,y = pyautogui.position()
+print(f'x: {x}, y: {y}')
---
+++ rpa_demo_1.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+time.sleep(5)
+
+pyautogui.click(988, 309)
+'''
+pyautogui.doubleclick(x, y)
+pyautogui.rightClick(x, y)
+pyautogui.scrolldown(500)
+pyautogui.write("hello world")
+pyautogui.press("enter")
+'''
+pyautogui.hotkey("ctrl", "a")
